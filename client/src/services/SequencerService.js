5e35944a00fe13a155d09e31f8eaf512fcea41d7
Stephen Herd <stephen_h369@live.co.uk>
2021-06-28 21:58:43 +0100

    updated for deployment

--- client/src/services/SequencerService.js
+++ client/src/services/SequencerService.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:3001/api/sequences/'
+const baseURL = '/api/sequences/'
 
 export default {
     getSequences(){
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const parser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const uri = process.env.MONGODB_URI;
 const createRouter = require("./helpers/create_router");
 
 // Create server and set it to use cors and body-parser
@@ -10,12 +11,19 @@ app.use(cors());
 app.use(parser.json());
 
 // Set up database connection and routers
-MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
+MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(client => {
     const db = client.db("step_sequencer");
     const sequencesCollection = db.collection("sequences");
     const sequencesRouter = createRouter(sequencesCollection);
     app.use("/api/sequences", sequencesRouter);
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(__dirname + '/public'));
+      app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  
+      let port_number = process.env.PORT || 3001;
+      app.listen(port_number);
+    }
   })
   .catch(console.error);
 
